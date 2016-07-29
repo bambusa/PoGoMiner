2e11af2da37deeda8bb8e30365be3b45c5f15167
bambusa <beny-ulms@gmx.de>
2016-07-29 02:40:21 +0200

    port

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(function(err, req, res, next) {
 
 var server = app.listen(65001, function (err) {
     if (err) throw err
-    console.log('Server running at http://0.0.0.0:5001/');
+    console.log('Server running at http://0.0.0.0:65001/');
 })
 
 
